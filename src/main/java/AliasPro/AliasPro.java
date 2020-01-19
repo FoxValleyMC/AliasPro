022ac83bd32b3b9ee7d5246c07839c07d71afb6f
Steve Coulter <stevecoulter99@gmail.com>
2020-01-19 14:37:54 -0600

    get player by name returns uuid

--- src/main/java/AliasPro/AliasPro.java
+++ src/main/java/AliasPro/AliasPro.java
@@ -2,6 +2,8 @@ package AliasPro;
 
 import AliasPro.Handler.DatabaseHandler;
 
+import java.util.Map;
+
 public class AliasPro {
 
     public static String getAlias(String uuid) {
@@ -10,6 +12,15 @@ public class AliasPro {
 
     public static String getNick(String uuid) {
         return DatabaseHandler.query(uuid, "uuid").get("alias").toString();
+    }
+
+    public static String getPlayer (String name) {
+        Map<String, Object> objectMap = DatabaseHandler.query(name, "name");
+        if (name == null) {
+            return null;
+        } else {
+            return objectMap.get("uuid").toString();
+        }
     }
 
 }
